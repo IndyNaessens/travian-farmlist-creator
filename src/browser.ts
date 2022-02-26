3c918c2e9b9a60c090458415d810a60bc29e3577
Indy Naessens <indy.naessens@pm.me>
2022-02-26 14:23:16 +0100

    Decrease slowMo

--- src/browser.ts
+++ src/browser.ts
@@ -3,7 +3,7 @@ import { Browser, chromium, Page } from 'playwright';
 export const startBrowserAndOpenPage = async (): Promise<[Browser, Page]> => {
     const browser = await chromium.launch({
         headless: true,
-        slowMo: 1000,
+        slowMo: 700,
         chromiumSandbox: true
     });
 
